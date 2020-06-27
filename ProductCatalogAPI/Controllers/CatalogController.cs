8605d88c4f599cf8ca58f7c1028df663d5c3616c
sonali-mhihim <63938232+sonali-mhihim@users.noreply.github.com>
2020-06-27 12:46:13 -0700

    Add pagination

--- ProductCatalogAPI/Controllers/CatalogController.cs
+++ ProductCatalogAPI/Controllers/CatalogController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using ProductCatalogAPI.Data;
+using ProductCatalogAPI.Domain;
+using ProductCatalogAPI.ViewModels;
 
 namespace ProductCatalogAPI.Controllers
 {
@@ -13,24 +17,91 @@ namespace ProductCatalogAPI.Controllers
     [ApiController]
     public class CatalogController : ControllerBase
     {
-        private readonly CatalogContext context;
-        private CatalogContext _context;
-
-        public CatalogController(CatalogContext context)
+        private readonly CatalogContext _context;
+        private readonly IConfiguration _config;
+        public CatalogController(CatalogContext context, IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
         [HttpGet("[action]")]
         public async Task<IActionResult> Items(
             [FromQuery]int pageIndex = 0,
-            [FromQuery]int pagesize =6)
-       {
-          var items = await _context.CatalogItems
-                .OrderBy(c => c.Name)
-                .Skip(pageIndex * pagesize)
-                .Take(pagesize)
-                .ToListAsync();
-            return Ok(items);
+            [FromQuery]int pageSize = 6)
+        {
+            var itemsCount = await _context.CatalogItems.LongCountAsync();
+
+            var items = await _context.CatalogItems
+                  .OrderBy(c => c.Name)
+                  .Skip(pageIndex * pageSize)
+                  .Take(pageSize)
+                  .ToListAsync();
+            items = ChangePictureUrl(items);
+            var model = new PaginatedItemsViewModel<CatalogItem>
+            {
+                PageIndex = pageIndex,
+                PageSize = items.Count,
+                Count = itemsCount,
+                Data = items,
+            };
+            return Ok(model);
+        }
+
+        private List<CatalogItem> ChangePictureUrl(List<CatalogItem> items)
+        {
+            items.ForEach(item =>
+           item.PictureUrl = item.PictureUrl.Replace(
+                             "http://externalcatalogbaseurltobereplaced",
+                              _config["ExternalCatalogBaseUrl"]));
+            return items;
+        }
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> CatalogTypes()
+        {
+            var types = await _context.CatalogTypes.ToListAsync();
+            return Ok(types);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> CatalogBrands()
+        {
+            var brands = await _context.CatalogBrands.ToListAsync();
+            return Ok(brands);
+        }
+        [HttpGet("[action]/type/{catalogTypeId}/brand/{ catalogBrandId}")]
+        public async Task<IActionResult> Items(
+            int? catalogTypeId,
+            int? catalogBrandId,
+           [FromQuery] int pageIndex = 0,
+           [FromQuery] int pageSize = 6)
+        {
+            var query = (IQueryable<CatalogItem>)_context.CatalogItems;
+            if (catalogTypeId.HasValue)
+            {
+                query = query.Where(c => c.CatalogTypeId == catalogTypeId);
+            }
+            if (catalogBrandId.HasValue)
+            {
+                query = query.Where(c => c.CatalogBrandId == catalogBrandId);
+            }
+            var itemsCount = query.LongCountAsync();
+
+            var items = await query
+                  .OrderBy(c => c.Name)
+                  .Skip(pageIndex * pageSize)
+                  .Take(pageSize)
+                  .ToListAsync();
+            items = ChangePictureUrl(items);
+            var model = new PaginatedItemsViewModel<CatalogItem>
+            {
+                PageIndex = pageIndex,
+                PageSize = items.Count,
+                Count = itemsCount.Result,
+                Data = items,
+            };
+            return Ok(model);
+
         }
     }
 }
---
+++ ProductCatalogAPI/ViewModels/PaginatedItemsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ProductCatalogAPI.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogAPI.ViewModels
+{
+    public class PaginatedItemsViewModel<T>
+    {
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public long Count { get; set; }
+        public IEnumerable<T> Data { get; set; }
+
+    }
+}
