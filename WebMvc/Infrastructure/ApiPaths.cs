e6b3b0c1b1b5d56edd8f13b8419bf67ee0a3cbb9
sonali-mhihim <63938232+sonali-mhihim@users.noreply.github.com>
2020-07-27 12:02:28 -0700

    Created Infrasture for CartApi under webMVC

--- WebMvc/Infrastructure/ApiPaths.cs
+++ WebMvc/Infrastructure/ApiPaths.cs
@@ -35,4 +35,22 @@ namespace WebMvc.NewFolder
             }
         }
     }
-}
+
+    public static class Basket
+    {
+        public static string GetBasket(string baseUri, string basketId)
+        {
+            return $"{baseUri}/{basketId}";
+        }
+
+        public static string UpdateBasket(string baseUri)
+        {
+            return baseUri;
+        }
+
+        public static string CleanBasket(string baseUri, string basketId)
+        {
+            return $"{baseUri}/{basketId}";
+        }
+    }
+    }
